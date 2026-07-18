fb86f20c86635cab2097267704d789a193bb398e
Gian Raphael <gianraphaelbaldovino@gmail.com>
2026-07-18 23:12:10 +0800

    Changed private keys to ENV

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -1,6 +1,6 @@
 import { initializeApp, cert, ServiceAccount, getApp, getApps } from 'firebase-admin/app'
 import { getFirestore } from 'firebase-admin/firestore'
-import serviceAccount from '@/serviceAccountKey.json';
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY!)
 
 const app = getApps().length == 0 ? initializeApp({ credential: cert(serviceAccount as ServiceAccount) }) : getApp()
 
